1fdb0f99d22e96257166f327a3f55a50d267f091
Krairic <wagarenai@gmail.com>
2023-01-17 16:09:38 +0100

    commit all

--- src/components/Main.js
+++ src/components/Main.js
@@ -18,33 +18,6 @@ function ArtistCard() {
         </p>
     )
 }     
-        
-
-// function Joke() {
-//     const array = [{name: "dicks",
-//                     age: 6,
-//                     motivation: "none"},
-//                    {name: "chode",
-//                     age: 99,
-//                     motivation: "still none"},
-//                     {name: "test",
-//                     age: 88,
-//                     motivation: "never"}]
-
-//     return (
-//         <p>
-//         {array.map(item => {
-//             return (
-//                 <div>
-//             <h1>{item.name}</h1>
-//             <h2>{item.age}</h2>
-//             <h3>{item.motivation}</h3>
-//             </div>
-//         )})}
-        
-//         </p>
-//     )
-// }
 
 function Main() {
 
--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./style.css"
-import Header from "./components/Header";
-import Main from "./components/Main";
+import "./styles/style.css"
+import "./styles/header-style.css"
+import App from "./app.js"
 import "./images/Logo-transparant-white.png"
-import TopImage from "./components/top-image";
+
 
 ReactDOM.render(
-    <TopImage />,
-    document.getElementById("top-image")
-)
-
-ReactDOM.render(
-    <Header />,
-    document.getElementById("root")
-)
-
-ReactDOM.render(
-    <Main />,
-    document.getElementById("root2")
-)
+    <App />,
+    document.getElementById("root"))
