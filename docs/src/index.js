5d6a187391bf52f4a9053c21413113b812fa73e6
Krairic <wagarenai@gmail.com>
2023-02-16 18:13:22 +0100

    y

--- docs/src/index.js
+++ docs/src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 
 ReactDOM.render(
-    <BrowserRouter basename="/docs">
+    <BrowserRouter>
     <App />
     </BrowserRouter>,
     document.getElementById("root"))
