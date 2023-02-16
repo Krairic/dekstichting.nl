b4340b190d6cfaf5d842a8b2026dd888fc672956
Krairic <wagarenai@gmail.com>
2023-02-16 16:22:59 +0100

    y

---
+++ src/Data/AgendaData.js
@@ -0,0 +1,14 @@
+const agendaData = [{
+    titel: "Finissage Ema Vanekova",
+    datum: "30 juni 2022",
+    body: "Op 30 juni vanaf 19:30 zal de finissage plaatsvinden van onze achtste residente Ema Vanekova in Cafe De Rooie Haen. Zij heeft drie weken geschilderd en heet iedereen van harte welkom om te komen kijken en met haar te spreken.",
+    img: null
+},
+{
+    titel: "Opening presentatie Nikki Nordman in De Rooie Haen",
+    datum: "1 juni 2022",
+    body: "Op 1 juni, vanaf 17:00, is het werk van Nikki Nordman te zien bij De Rooie Haen. Nikki heeft twee weken gewerkt met fotografie, de resultaten zullen te bekijken zijn en de kunstenaar te spreken.",
+    img: null
+}]
+
+export default agendaData
---
+++ src/pages/Contact.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function Contact() {
+    return (
+
+    <div className="contact--wrapper">
+    <h3>info@dekstichting.nl</h3>
+    <p>Voor vragen, interesse of verzoeken zijn wij op de mail te bereiken. <br />
+    Een prive bericht op Instagram is ook altijd mogelijk maar kan eventueel pas later gezien worden.</p>
+    <br />    
+    <p><a id="contact--socialLinks" href="https://instagram.com/dekstichting">DeKStichting op Instagram</a></p>
+    <p><a id="contact--socialLinks" href="https://instagram.com/air.rooie.haen">AiR de Rooie Haen op Instagram</a></p>
+    </div>
+)}
+
+export default Contact
